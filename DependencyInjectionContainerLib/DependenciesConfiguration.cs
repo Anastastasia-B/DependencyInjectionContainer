d973aa18b58045e28167897496e7fd9d879274a2
Nastya Ber <anastasiaberz212@gmail.com>
2022-01-03 17:18:37 +0300

    create base lib structure

---
+++ DependencyInjectionContainerLib/DependenciesConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DependencyInjectionContainerLib
+{
+    public class DependenciesConfiguration
+    {
+        public DependenciesConfiguration()
+        {
+            
+        }
+
+        public void Register<T, F>()
+        {
+            Register(typeof(T), typeof(F));
+        }
+
+        public void Register(Type dependencyType, Type implementationType)
+        {
+        }
+    }
+}
---
+++ DependencyInjectionContainerLib/DependencyProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DependencyInjectionContainerLib
+{
+    public class DependencyProvider
+    {
+        private DependenciesConfiguration _dependencies;
+        public DependencyProvider(DependenciesConfiguration dependencies)
+        {
+            _dependencies = dependencies;
+        }
+
+        public T Resolve<T>()
+        {
+            return default;
+        }
+    }
+}
