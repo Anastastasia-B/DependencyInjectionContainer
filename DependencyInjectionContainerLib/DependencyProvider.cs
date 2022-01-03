8a0437d632082d5843b413bd79fa93018c8021af
Nastya Ber <anastasiaberz212@gmail.com>
2022-01-03 17:30:06 +0300

    implement dependencies creation

--- DependencyInjectionContainerLib/DependencyProvider.cs
+++ DependencyInjectionContainerLib/DependencyProvider.cs
@@ -3,6 +3,8 @@ using System.Linq;
 using System.Reflection;
 using System.Collections;
 using System.Collections.Generic;
+using DependencyInjectionContainerLib.Reflection;
+using DependencyInjectionContainerLib.Implementation;
 
 namespace DependencyInjectionContainerLib
 {
@@ -16,7 +18,43 @@ namespace DependencyInjectionContainerLib
 
         public T Resolve<T>()
         {
-            return default;
+            return (T)Resolve(typeof(T), false);
+        }
+
+        private object Resolve(Type type, bool createAllImplementations)
+        {
+            object instance = null;
+            List<Type> implementations = null;
+            IDependencyLife dependencyLifeObject = null;
+            if (createAllImplementations)
+            {
+                instance = Activator.CreateInstance(typeof(List<>).MakeGenericType(type));
+                for (int i = 0; i < implementations.Count; i++)
+                {
+                    dependencyLifeObject = (IDependencyLife)ObjectCreator.CreateInstance(implementations[i], type.GetGenericArguments());
+                    (instance as IList).Add(dependencyLifeObject.GetInstance(GetConstructorParams(implementations[i].GenericTypeArguments[0])));
+                }
+            }
+            else
+            {
+                dependencyLifeObject = (IDependencyLife)ObjectCreator.CreateInstance(implementations[0], type.GetGenericArguments());
+                instance = dependencyLifeObject.GetInstance(GetConstructorParams(implementations[0].GenericTypeArguments[0]));
+            }
+            return instance;
+        }
+
+        private object[] GetConstructorParams(Type type)
+        {
+            List<object> constructorParams = new List<object>();
+            ConstructorInfo constructor = type.GetConstructors().OrderByDescending(con => con.GetParameters().Length).First();
+
+            ParameterInfo[] parameters = constructor.GetParameters();
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                constructorParams.Add(Resolve(parameters[i].ParameterType, false));
+            }
+
+            return constructorParams.ToArray();
         }
     }
 }
---
+++ DependencyInjectionContainerLib/Reflection/ObjectCreator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyInjectionContainerLib.Reflection
+{
+    internal static class ObjectCreator
+    {
+        internal static object CreateInstance(Type type, object[] constructorParams)
+        {
+            return CreateInstance(type, Array.Empty<Type>(), constructorParams);
+        }
+
+        internal static object CreateInstance(Type type, Type[] genericArguments)
+        {
+            return CreateInstance(type, genericArguments, Array.Empty<object>());
+        }
+
+        internal static object CreateInstance(Type type, Type[] genericArguments, object[] constructorParams)
+        {
+            return Activator.CreateInstance(CreateType(type, genericArguments), constructorParams);
+        }
+
+        private static Type CreateType(Type baseType, Type[] geneticArguments)
+        {
+            Type instance = baseType;
+            if (baseType.IsGenericTypeDefinition)
+            {
+                instance = baseType.MakeGenericType(geneticArguments);
+            }
+            else
+            {
+                if (baseType.IsGenericType)
+                {
+                    Type[] currentStepGenericArgs = baseType.GetGenericArguments().Select(arg => CreateType(arg, geneticArguments)).ToArray();
+                    instance = baseType.GetGenericTypeDefinition().MakeGenericType(currentStepGenericArgs);
+                }
+            }
+            return instance;
+        }
+    }
+}
